5ffb2970ce375eeb42fba6a9b96afd3f768cea09
gyeongminpark <mh7840@naver.com>
2024-10-29 17:17:26 +0900

    commit message

--- src/js/aboutme.js
+++ src/js/aboutme.js
@@ -2,7 +2,6 @@ import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 
 export function aboutme() {
-    // ScrollTrigger 플러그인 등록
     gsap.registerPlugin(ScrollTrigger);
 
     const sections = [
@@ -14,16 +13,14 @@ export function aboutme() {
         document.querySelector(".ab_key")
     ];
 
-    // 현재 보여질 섹션 인덱스
     let currentSection = 0;
 
-    // 스크롤할 때마다 하나의 섹션만 표시
     function showNextSection() {
         if (currentSection < sections.length) {
             const section = sections[currentSection];
             gsap.fromTo(
                 section,
-                { opacity: 0, y: 50 }, // 초기 상태
+                { opacity: 0, y: 50 },
                 {
                     opacity: 1,
                     y: 0,
@@ -32,12 +29,11 @@ export function aboutme() {
                     onComplete: () => {
                         currentSection++;
                         if (currentSection < sections.length) {
-                            // 다음 섹션을 스크롤 트리거로 설정
                             ScrollTrigger.create({
                                 trigger: sections[currentSection],
                                 start: "top 80%",
                                 onEnter: showNextSection,
-                                once: true // 한 번만 실행되도록 설정
+                                once: true
                             });
                         }
                     }
@@ -46,7 +42,6 @@ export function aboutme() {
         }
     }
 
-    // 첫 번째 섹션에 대한 초기 ScrollTrigger 설정
     ScrollTrigger.create({
         trigger: sections[0],
         start: "top 80%",
